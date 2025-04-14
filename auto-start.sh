ce3528e1b422475d2ee7aa434a68cf1846df90da
kingelvyn <60129736+kingelvyn@users.noreply.github.com>
2025-04-14 02:25:00 -0500

    Update auto-start.sh
    Added some system messages and updated paths for automatically starting programs

--- auto-start.sh
+++ auto-start.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
-
-source /home/elvyn/hk-arial/venv_hkarial/bin/activate #activate virtual environment 
-
-python /home/elvyn/hk-arial/live_inference.py -m "/home/elvyn/hk-arial/result-v3-yolo11/best_openvino_2022.1_6shave.blob" -c "/home/elvyn/hk-arial/result-v3-yolo11/best.json" # running program with appropriate parameters   
+#!/bin/bash
+echo "Activating virtual environment..."
+source /home/hkarial/hk-arial/venv_hkarial/bin/activate # activate virtual environment
+echo "Starting the controller.py program..."
+python /home/hkarial/hk-arial/controller.py # run the controller script
