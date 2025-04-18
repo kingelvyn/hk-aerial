58dba56a622c4def0816a66e6d69772415398c73
kingelvyn <60129736+kingelvyn@users.noreply.github.com>
2025-04-18 00:42:09 -0500

    Update controller.py
    Improved upon logging for mavlink files

--- controller.py
+++ controller.py
@@ -16,15 +16,23 @@ from pymavlink import mavutil
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 MAVLINK_CONNECTION = "/dev/ttyAMA0"  
 LOG_DIR = "/home/hkarial/hk-arial/logs/pixhawk_logs" 
-LOG_FILE = f"{LOG_DIR}/controller_{timestamp}.log" 
+CONTROLLER_LOG_DIR = "/home/hkarial/hk-arial/logs/controller_logs"
+LOG_FILE = f"{CONTROLLER_LOG_DIR}/controller_{timestamp}.log" 
 OBJECT_DETECTION_SCRIPT = "/home/hkarial/hk-arial/live_inference.py" 
-LOGGING_SCRIPT = "/home/hkarial/hk-arial/pixhawk_logs.py" 
+#LOGGING_SCRIPT = "/home/hkarial/hk-arial/pixhawk_logs.py" # Deprecated
 MODEL_PATH = "/home/hkarial/hk-arial/result-v3-yolo11/best_openvino_2022.1_6shave.blob"
 CONFIG_PATH = "/home/hkarial/hk-arial/result-v3-yolo11/best.json"
 master = mavutil.mavlink_connection(MAVLINK_CONNECTION, baud=921600)
 
 # Setup logging 
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(message)s') 
+
+# Logging network connection
+#with open("/home/hkarial/hk-arial/logs/network_log.txt", "a") as log:
+#	log.write(f"[{timestamp}] Startup:n")
+#	log.write(os.popen("date").read())
+#	log.write(os.popen("ip addr").read())
+#	log.write("\n\n")
 
 # Connect to MAVLink for ARM check
 logging.info("Connecting to MAVLink...") 
@@ -54,31 +62,41 @@ def stop_scripts():
 
 # Main loop
 armed = False
+mavlinkFile_handle = None
 try: 
     logging.info("Starting controller loop...") 
     while True: 
         msg = master.recv_match(type="HEARTBEAT", blocking=True, timeout=1) 
+        # Logging raw mavlink data
         if msg: 
             is_armed = msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED 
             if is_armed and not armed: 
                 log_file = os.path.join(LOG_DIR, f"flightlog_{timestamp}.tlog")
-                master.logfile_raw = open(log_file, 'wb')
+                mavlinkFile_handle = open(log_file, 'wb')
+                master.logfile_raw = mavlinkFile_handle
                 logging.info("Drone armed - logging started.")
                 start_scripts()
                 armed = True
             elif not is_armed and armed: 
                 if master.logfile_raw:
+                    master.logfile_raw.flush()
                     master.logfile_raw.close()
                     master.logfile_raw = None
+                mavlinkFile_handle = None
                 logging.info("Drone disarmed - logging stopping.")
                 stop_scripts()
                 armed = False
-        time.sleep(0.5)
-
+        time.sleep(0.3)
+        
+# Excepts for graceful shutdowns to prevent corruption
 except KeyboardInterrupt: 
     logging.info("Shutting down due to KeyboardInterrupt.") 
     stop_scripts() 
+    if master.logfile_raw:
+        master.logfile_raw.close()
 
 except Exception as e: 
     logging.error("Exception occurred: %s", e) 
     stop_scripts()
+    if master.logfile_raw:
+        master.logfile_raw.close()
